4038b24f106ca4998581408d649394d0227c9881
Robert (R.G.) Dinu <robert.dinu@ing.com>
2020-10-30 14:45:41 +0100

    Added init to archive.

--- build-pipeline/package.sh
+++ build-pipeline/package.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 rm -f ../package.zip
-zip -r ../package.zip ../dataproc/github_insights_pyspark.py ../dataproc/start_pyspak.py   -x run.py \*.pyc \*__pycache__* \*.sh
+zip -r ../package.zip ../dataproc/github_insights_pyspark.py ../dataproc/start_pyspak.py ../dataproc/__init__.py  -x \*.pyc \*__pycache__*
 cd ..
